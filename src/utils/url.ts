8e4e1246368524c606e2a9914957bf057c51e760
Luchko Aleksandr <luchcko.a@gmail.com>
2023-07-26 11:27:28 +0300

    fix url to minio

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -2,7 +2,7 @@ import { BUCKET_PICTURE_NAME } from 'src/constants';
 
 export function getMinioObjectUrl(name: string, bucket = BUCKET_PICTURE_NAME): string {
   if (process.env.NODE_ENV === 'production') {
-    return `https://${bucket}.${process.env.MINIO_URL}/${name}`;
+    return `https://${process.env.MINIO_URL}/${bucket}/${name}`;
   }
   return `http://${process.env.MINIO_URL}/${bucket}/${name}`;
 }
